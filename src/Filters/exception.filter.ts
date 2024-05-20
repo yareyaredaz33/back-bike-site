43167fdd33697bbfa07c7364fd26452c9e3c0952
yareyaredaz33 <lovica15@gmail.com>
2024-05-20 15:55:42 +0300

    Added filters for page

---
+++ src/Filters/exception.filter.ts
@@ -0,0 +1,35 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    switch (status) {
+      case 400:
+        const errorResponse = {
+          errorsMessages: [],
+        };
+        const responseBody: any = exception.getResponse();
+        console.log(responseBody);
+        if (Array.isArray(responseBody.message)) {
+          responseBody.message?.forEach((m) =>
+            errorResponse.errorsMessages.push(m),
+          );
+        }
+
+        response.status(status).json(errorResponse);
+        break;
+      default:
+        response.sendStatus(status);
+        break;
+    }
+  }
+}
